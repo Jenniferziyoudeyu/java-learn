fa09098f2100e4c77d81bae2a0d91b9e0c4d94f6
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-01 12:43:44 -0700

    changes

--- src/Main.java
+++ src/Main.java
@@ -33,8 +33,9 @@ public class Main {
 
         SortAlgorithms sort = new SortAlgorithms();
         int[] array = new int[]{2, 1, 0, 5, 7, 9, 3, 4};
-        sort.insertSort(array);
+//        sort.insertSort(array);
 //        sort.selectionSort(array);
+        mergeSort(array);
         printArray(array);
 
         int[] arr = new int[2];
@@ -61,6 +62,45 @@ public class Main {
             System.out.print(array[i] + ",");
         }
         System.out.println();
+    }
+
+    static void mergeSort(int[] array) {
+        mergeSortHelper(array, 0, array.length - 1, new int[array.length]);
+    }
+
+    private static void mergeSortHelper(int[] array, int start, int end, int[] temp) {
+        if (start >= end) return; //设置终止条件。如果没有这句，就是一直执行下去。
+        //找到中点，平均分成两个部分。以下三行执行完，就把
+        int mid = start + (end - start)/2;
+        //以下两行是两个recursion，就是递归
+        mergeSortHelper(array, start, mid, temp);
+        /*
+        在start = end之前一直执行前两句代码。直到start = end 代码return到上一层
+        执行 mergeSortHelper(array, start, mid, temp);
+        之后开始执行下面一句 所以都是先把靠左的路线走完再返回去走右边的
+         */
+        mergeSortHelper(array, mid + 1, end, temp);
+        //此时已经是排好序的两组array。以下是两个array中的角标
+        int i = start, j = mid + 1, index = start;
+        while (i <= mid || j <= end) {
+            int num1 = i <= mid ? array[i] : Integer.MAX_VALUE;
+            int num2 = j <= end ? array[j] : Integer.MAX_VALUE;
+            if (num1 < num2) {
+                /*
+                以下一句是这两行代码的简写
+                temp[index] = num1;
+                index++；
+                 */
+                temp[index++] = num1;
+                i++;
+            } else {
+                temp[index++] = num2;
+                j++;
+            }
+        }
+        for (int k = start; k <= end; k++) {
+            array[k] = temp[k];
+        }
     }
 }
 
---
+++ src/laiOffer/Day1Array.java
@@ -0,0 +1,12 @@
+package laiOffer;
+
+public class Day1Array {
+    //打印int array里的所有元素
+    public void print(int[] array) {
+        if (array == null || array.length == 0)
+            return;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+    }
+}
