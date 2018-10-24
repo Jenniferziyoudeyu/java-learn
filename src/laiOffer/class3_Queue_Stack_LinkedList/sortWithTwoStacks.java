5189ad860880831e18e2e6b20b1d35d18bbb0c27
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-24 16:23:16 -0700

    sort with 2 stacks todo

---
+++ src/laiOffer/class3_Queue_Stack_LinkedList/sortWithTwoStacks.java
@@ -0,0 +1,28 @@
+package laiOffer.class3_Queue_Stack_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Description
+ * Given an array that is initially stored in one stack, sort it with one additional stacks (total 2 stacks).
+ *
+ * After sorting the original stack should contain the sorted integers and from top to bottom the integers are sorted in ascending order.
+ *
+ * Assumptions:
+ *
+ * The given stack is not null.
+ * Requirements:
+ *
+ * No additional memory, time complexity = O(n ^ 2).
+ */
+
+//TODO: https://app.laicode.io/app/problem/280
+
+public class sortWithTwoStacks {
+    public void sort(LinkedList<Integer> s1) {
+        List<Integer> s2 = new LinkedList<>();
+        // Write your solution here.
+
+    }
+}
