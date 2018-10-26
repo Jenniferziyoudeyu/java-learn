447cd9de78f9cbe8ce449707d35d22559cda9c99
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-25 23:12:33 -0700

    insert in sorted linked list

--- src/laiOffer/class3_Queue_Stack_LinkedList/InsertInSortedLinkedList.java
+++ src/laiOffer/class3_Queue_Stack_LinkedList/InsertInSortedLinkedList.java
@@ -2,6 +2,8 @@ package laiOffer.class3_Queue_Stack_LinkedList;
 
 import laiOffer.ListNode;
 
+import java.util.List;
+
 /**
  *
  Description
@@ -15,9 +17,51 @@ import laiOffer.ListNode;
  L = 2 -> 3 -> null, insert 1, return 1 -> 2 -> 3 -> null
 
  */
-
+/*
+  public ListNode insert(ListNode head, int value) {
+        ListNode newNode = new ListNode(value);
+        ListNode pre = null, cur = head;
+        while (cur != null && cur.value < value) {
+            pre = cur;
+            cur = cur.next;
+        }
+        newNode.next = cur;
+        if (pre == null) return newNode;
+        else pre.next = newNode;
+        return head;
+    }
+ */
 public class InsertInSortedLinkedList {
     public ListNode insert(ListNode head, int value) {
-        if (head == null)
+        if (head == null) return new ListNode(value);
+        ListNode res = head;
+        ListNode prev = null;
+        while (head != null && head.value < value) {
+            prev = head;
+            head = head.next;
+        }
+        ListNode insertNode = new ListNode(value);
+        if (prev == null) {
+            insertNode.next = res;
+            return insertNode;
+        }
+        prev.next = insertNode;
+        insertNode.next = head;
+        return res;
     }
+
+    public ListNode insert2(ListNode head, int value) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null && head.value < value) {
+            pre = head;
+            head = head.next;
+        }
+        ListNode newNode = new ListNode(value);
+        pre.next = newNode;
+        newNode.next = head;
+        return dummy.next;
+    }
+
 }
